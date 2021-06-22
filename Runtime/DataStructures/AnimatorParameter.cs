539c687060aebacf82e7327c2b2d22b3e4039018
Adam Graham <adam@zigurous.com>
2021-06-22 13:22:46 -0500

    Add AnimatorParameter data structure for managing hashes

---
+++ Runtime/DataStructures/AnimatorParameter.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+namespace Zigurous.Animation
+{
+    /// <summary>
+    /// An animation parameter that can be set on an animator. A hash id is
+    /// automatically created for the parameter for optimal code.
+    /// </summary>
+    [System.Serializable]
+    public struct AnimatorParameter
+    {
+        [SerializeField]
+        [HideInInspector]
+        private int _hash;
+
+        /// <summary>
+        /// The hash id of the animation parameter.
+        /// </summary>
+        public int hash => _hash;
+
+        [SerializeField]
+        [Tooltip("The name of the animation parameter.")]
+        private string _name;
+
+        /// <summary>
+        /// The name of the animation parameter.
+        /// </summary>
+        public string name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                _hash = Animator.StringToHash(value);
+            }
+        }
+
+        /// <summary>
+        /// Constructs a new animation parameter with the given name.
+        /// </summary>
+        public AnimatorParameter(string name)
+        {
+            _name = name;
+            _hash = Animator.StringToHash(name);
+        }
+
+        /// <summary>
+        /// Generates a new hash for the parameter using the current name.
+        /// </summary>
+        public int GenerateHash()
+        {
+            _hash = Animator.StringToHash(_name);
+            return _hash;
+        }
+
+        public static implicit operator AnimatorParameter(string name) => new AnimatorParameter(name);
+
+    }
+
+}

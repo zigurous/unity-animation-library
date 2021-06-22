4325b71dc9a13ffe390134ee557bad6dc29e0491
Adam Graham <adam@zigurous.com>
2021-06-22 13:36:48 -0500

    Add editor property drawer for AnimatorParameter

---
+++ Editor/AnimatorParameterPropertyDrawer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Zigurous.Animation.Editor
+{
+    [CustomPropertyDrawer(typeof(AnimatorParameter))]
+    public sealed class AnimatorParameterPropertyDrawer : PropertyDrawer
+    {
+        private SerializedProperty _name;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (_name == null) {
+                _name = property.FindPropertyRelative("_name");
+            }
+
+            EditorGUI.BeginProperty(position, label, property);
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+            _name.stringValue = EditorGUI.TextField(position, _name.stringValue);
+            EditorGUI.EndProperty();
+        }
+
+    }
+
+}

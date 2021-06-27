5bbe93a397d302a340b289e7a307c9088e553919
Adam Graham <adam@zigurous.com>
2021-06-27 01:35:10 -0500

    Separate speed from rotation axis

--- Runtime/Rotate.cs
+++ Runtime/Rotate.cs
@@ -8,10 +8,16 @@ namespace Zigurous.Animation
     public sealed class Rotate : MonoBehaviour
     {
         /// <summary>
+        /// The axis in which the object rotates.
+        /// </summary>
+        [Tooltip("The axis in which the object rotates.")]
+        public Vector3 axis = Vector3.up;
+
+        /// <summary>
         /// The speed at which the object rotates.
         /// </summary>
         [Tooltip("The speed at which the object rotates.")]
-        public Vector3 speed = Vector3.up;
+        public float speed = 1.0f;
 
         /// <summary>
         /// The coordinate space in which the object rotates.
@@ -28,21 +34,21 @@ namespace Zigurous.Animation
         private void Update()
         {
             if (this.updateMode == UpdateMode.Update) {
-                this.transform.Rotate(this.speed * Time.deltaTime, this.space);
+                this.transform.Rotate(this.axis * this.speed * Time.deltaTime, this.space);
             }
         }
 
         private void LateUpdate()
         {
             if (this.updateMode == UpdateMode.LateUpdate) {
-                this.transform.Rotate(this.speed * Time.deltaTime, this.space);
+                this.transform.Rotate(this.axis * this.speed * Time.deltaTime, this.space);
             }
         }
 
         private void FixedUpdate()
         {
             if (this.updateMode == UpdateMode.FixedUpdate) {
-                this.transform.Rotate(this.speed * Time.fixedDeltaTime, this.space);
+                this.transform.Rotate(this.axis * this.speed * Time.fixedDeltaTime, this.space);
             }
         }
 

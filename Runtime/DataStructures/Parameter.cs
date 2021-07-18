176b8247ad1d3043e659b8f06f1ffd134417b1d2
Adam Graham <adam@zigurous.com>
2021-07-18 02:07:52 -0500

    Add constant parameter ids

---
+++ Runtime/DataStructures/Parameter.cs
@@ -0,0 +1,521 @@
+﻿using UnityEngine;
+
+namespace Zigurous.Animation
+{
+    /// <summary>
+    /// Constant animator parameter hash ids.
+    /// </summary>
+    public static class Parameter
+    {
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Aim")</c>.
+        /// </summary>
+        public static readonly int Aim = Animator.StringToHash("Aim");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Airborne")</c>.
+        /// </summary>
+        public static readonly int Airborne = Animator.StringToHash("Airborne");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Attack")</c>.
+        /// </summary>
+        public static readonly int Attack = Animator.StringToHash("Attack");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Backward")</c>.
+        /// </summary>
+        public static readonly int Backward = Animator.StringToHash("Backward");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Block")</c>.
+        /// </summary>
+        public static readonly int Block = Animator.StringToHash("Block");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Break")</c>.
+        /// </summary>
+        public static readonly int Break = Animator.StringToHash("Break");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Cast")</c>.
+        /// </summary>
+        public static readonly int Cast = Animator.StringToHash("Cast");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Catch")</c>.
+        /// </summary>
+        public static readonly int Catch = Animator.StringToHash("Catch");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Climb")</c>.
+        /// </summary>
+        public static readonly int Climb = Animator.StringToHash("Climb");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Close")</c>.
+        /// </summary>
+        public static readonly int Close = Animator.StringToHash("Close");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Combo")</c>.
+        /// </summary>
+        public static readonly int Combo = Animator.StringToHash("Combo");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Cover")</c>.
+        /// </summary>
+        public static readonly int Cover = Animator.StringToHash("Cover");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Crawl")</c>.
+        /// </summary>
+        public static readonly int Crawl = Animator.StringToHash("Crawl");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Crouch")</c>.
+        /// </summary>
+        public static readonly int Crouch = Animator.StringToHash("Crouch");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Dance")</c>.
+        /// </summary>
+        public static readonly int Dance = Animator.StringToHash("Dance");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Dash")</c>.
+        /// </summary>
+        public static readonly int Dash = Animator.StringToHash("Dash");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Death")</c>.
+        /// </summary>
+        public static readonly int Death = Animator.StringToHash("Death");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Defeat")</c>.
+        /// </summary>
+        public static readonly int Defeat = Animator.StringToHash("Defeat");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Defend")</c>.
+        /// </summary>
+        public static readonly int Defend = Animator.StringToHash("Defend");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Die")</c>.
+        /// </summary>
+        public static readonly int Die = Animator.StringToHash("Die");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Dodge")</c>.
+        /// </summary>
+        public static readonly int Dodge = Animator.StringToHash("Dodge");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("DirectionX")</c>.
+        /// </summary>
+        public static readonly int DirectionX = Animator.StringToHash("DirectionX");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("DirectionY")</c>.
+        /// </summary>
+        public static readonly int DirectionY = Animator.StringToHash("DirectionY");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Disarm")</c>.
+        /// </summary>
+        public static readonly int Disarm = Animator.StringToHash("Disarm");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Down")</c>.
+        /// </summary>
+        public static readonly int Down = Animator.StringToHash("Down");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Drop")</c>.
+        /// </summary>
+        public static readonly int Drop = Animator.StringToHash("Drop");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Enter")</c>.
+        /// </summary>
+        public static readonly int Enter = Animator.StringToHash("Enter");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Equip")</c>.
+        /// </summary>
+        public static readonly int Equip = Animator.StringToHash("Equip");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Evade")</c>.
+        /// </summary>
+        public static readonly int Evade = Animator.StringToHash("Evade");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Examine")</c>.
+        /// </summary>
+        public static readonly int Examine = Animator.StringToHash("Examine");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Exit")</c>.
+        /// </summary>
+        public static readonly int Exit = Animator.StringToHash("Exit");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Fall")</c>.
+        /// </summary>
+        public static readonly int Fall = Animator.StringToHash("Fall");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Fight")</c>.
+        /// </summary>
+        public static readonly int Fight = Animator.StringToHash("Fight");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Fire")</c>.
+        /// </summary>
+        public static readonly int Fire = Animator.StringToHash("Fire");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Flip")</c>.
+        /// </summary>
+        public static readonly int Flip = Animator.StringToHash("Flip");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Focus")</c>.
+        /// </summary>
+        public static readonly int Focus = Animator.StringToHash("Focus");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Forward")</c>.
+        /// </summary>
+        public static readonly int Forward = Animator.StringToHash("Forward");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Gather")</c>.
+        /// </summary>
+        public static readonly int Gather = Animator.StringToHash("Gather");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Grab")</c>.
+        /// </summary>
+        public static readonly int Grab = Animator.StringToHash("Grab");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Grounded")</c>.
+        /// </summary>
+        public static readonly int Grounded = Animator.StringToHash("Grounded");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Hang")</c>.
+        /// </summary>
+        public static readonly int Hang = Animator.StringToHash("Hang");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Heal")</c>.
+        /// </summary>
+        public static readonly int Heal = Animator.StringToHash("Heal");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Hide")</c>.
+        /// </summary>
+        public static readonly int Hide = Animator.StringToHash("Hide");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Hit")</c>.
+        /// </summary>
+        public static readonly int Hit = Animator.StringToHash("Hit");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Hook")</c>.
+        /// </summary>
+        public static readonly int Hook = Animator.StringToHash("Hook");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Idle")</c>.
+        /// </summary>
+        public static readonly int Idle = Animator.StringToHash("Idle");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Inspect")</c>.
+        /// </summary>
+        public static readonly int Inspect = Animator.StringToHash("Inspect");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Jab")</c>.
+        /// </summary>
+        public static readonly int Jab = Animator.StringToHash("Jab");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Jog")</c>.
+        /// </summary>
+        public static readonly int Jog = Animator.StringToHash("Jog");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Jump")</c>.
+        /// </summary>
+        public static readonly int Jump = Animator.StringToHash("Jump");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Kick")</c>.
+        /// </summary>
+        public static readonly int Kick = Animator.StringToHash("Kick");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Kneel")</c>.
+        /// </summary>
+        public static readonly int Kneel = Animator.StringToHash("Kneel");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Land")</c>.
+        /// </summary>
+        public static readonly int Land = Animator.StringToHash("Land");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Lay")</c>.
+        /// </summary>
+        public static readonly int Lay = Animator.StringToHash("Lay");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Left")</c>.
+        /// </summary>
+        public static readonly int Left = Animator.StringToHash("Left");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Lift")</c>.
+        /// </summary>
+        public static readonly int Lift = Animator.StringToHash("Lift");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Look")</c>.
+        /// </summary>
+        public static readonly int Look = Animator.StringToHash("Look");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Loop")</c>.
+        /// </summary>
+        public static readonly int Loop = Animator.StringToHash("Loop");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Move")</c>.
+        /// </summary>
+        public static readonly int Move = Animator.StringToHash("Move");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Open")</c>.
+        /// </summary>
+        public static readonly int Open = Animator.StringToHash("Open");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Pass")</c>.
+        /// </summary>
+        public static readonly int Pass = Animator.StringToHash("Pass");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Peek")</c>.
+        /// </summary>
+        public static readonly int Peek = Animator.StringToHash("Peek");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Pickup")</c>.
+        /// </summary>
+        public static readonly int Pickup = Animator.StringToHash("Pickup");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Pivot")</c>.
+        /// </summary>
+        public static readonly int Pivot = Animator.StringToHash("Pivot");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Prone")</c>.
+        /// </summary>
+        public static readonly int Prone = Animator.StringToHash("Prone");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Pull")</c>.
+        /// </summary>
+        public static readonly int Pull = Animator.StringToHash("Pull");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Punch")</c>.
+        /// </summary>
+        public static readonly int Punch = Animator.StringToHash("Punch");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Push")</c>.
+        /// </summary>
+        public static readonly int Push = Animator.StringToHash("Push");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Reaction")</c>.
+        /// </summary>
+        public static readonly int Reaction = Animator.StringToHash("Reaction");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Ready")</c>.
+        /// </summary>
+        public static readonly int Ready = Animator.StringToHash("Ready");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Receive")</c>.
+        /// </summary>
+        public static readonly int Receive = Animator.StringToHash("Receive");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Reload")</c>.
+        /// </summary>
+        public static readonly int Reload = Animator.StringToHash("Reload");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Right")</c>.
+        /// </summary>
+        public static readonly int Right = Animator.StringToHash("Right");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Roll")</c>.
+        /// </summary>
+        public static readonly int Roll = Animator.StringToHash("Roll");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Run")</c>.
+        /// </summary>
+        public static readonly int Run = Animator.StringToHash("Run");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Sheath")</c>.
+        /// </summary>
+        public static readonly int Sheath = Animator.StringToHash("Sheath");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Shimmy")</c>.
+        /// </summary>
+        public static readonly int Shimmy = Animator.StringToHash("Shimmy");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Shoot")</c>.
+        /// </summary>
+        public static readonly int Shoot = Animator.StringToHash("Shoot");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Sit")</c>.
+        /// </summary>
+        public static readonly int Sit = Animator.StringToHash("Sit");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Slide")</c>.
+        /// </summary>
+        public static readonly int Slide = Animator.StringToHash("Slide");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Smash")</c>.
+        /// </summary>
+        public static readonly int Smash = Animator.StringToHash("Smash");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Sneak")</c>.
+        /// </summary>
+        public static readonly int Sneak = Animator.StringToHash("Sneak");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Speed")</c>.
+        /// </summary>
+        public static readonly int Speed = Animator.StringToHash("Speed");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Sprint")</c>.
+        /// </summary>
+        public static readonly int Sprint = Animator.StringToHash("Sprint");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Stab")</c>.
+        /// </summary>
+        public static readonly int Stab = Animator.StringToHash("Stab");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Stand")</c>.
+        /// </summary>
+        public static readonly int Stand = Animator.StringToHash("Stand");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Start")</c>.
+        /// </summary>
+        public static readonly int Start = Animator.StringToHash("Start");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Stop")</c>.
+        /// </summary>
+        public static readonly int Stop = Animator.StringToHash("Stop");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Strafe")</c>.
+        /// </summary>
+        public static readonly int Strafe = Animator.StringToHash("Strafe");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Swim")</c>.
+        /// </summary>
+        public static readonly int Swim = Animator.StringToHash("Swim");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Swing")</c>.
+        /// </summary>
+        public static readonly int Swing = Animator.StringToHash("Swing");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Take")</c>.
+        /// </summary>
+        public static readonly int Take = Animator.StringToHash("Take");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Taunt")</c>.
+        /// </summary>
+        public static readonly int Taunt = Animator.StringToHash("Taunt");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Throw")</c>.
+        /// </summary>
+        public static readonly int Throw = Animator.StringToHash("Throw");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Transition")</c>.
+        /// </summary>
+        public static readonly int Transition = Animator.StringToHash("Transition");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Turn")</c>.
+        /// </summary>
+        public static readonly int Turn = Animator.StringToHash("Turn");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Twist")</c>.
+        /// </summary>
+        public static readonly int Twist = Animator.StringToHash("Twist");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Unequip")</c>.
+        /// </summary>
+        public static readonly int Unequip = Animator.StringToHash("Unequip");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Up")</c>.
+        /// </summary>
+        public static readonly int Up = Animator.StringToHash("Up");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Vault")</c>.
+        /// </summary>
+        public static readonly int Vault = Animator.StringToHash("Vault");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Velocity")</c>.
+        /// </summary>
+        public static readonly int Velocity = Animator.StringToHash("Velocity");
+
+        /// <summary>
+        /// Equivalent to <c>Animator.StringToHash("Walk")</c>.
+        /// </summary>
+        public static readonly int Walk = Animator.StringToHash("Walk");
+    }
+
+}

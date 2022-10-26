ed23b302a55557a6f94ed7f8750895807377375b
Adam Graham <adam.graham@kinandcarta.com>
2022-10-25 23:57:28 -0500

    Hide update strategies from component menu

--- Runtime/UpdateStrategy/FixedUpdateStrategy.cs
+++ Runtime/UpdateStrategy/FixedUpdateStrategy.cs
@@ -5,6 +5,7 @@ namespace Zigurous.Animation
     /// <summary>
     /// An update strategy executed during the fixed update loop.
     /// </summary>
+    [AddComponentMenu("")]
     internal sealed class FixedUpdateStrategy : UpdateStrategy
     {
         private void FixedUpdate()
--- Runtime/UpdateStrategy/LateUpdateStrategy.cs
+++ Runtime/UpdateStrategy/LateUpdateStrategy.cs
@@ -5,6 +5,7 @@ namespace Zigurous.Animation
     /// <summary>
     /// An update strategy executed during the late update loop.
     /// </summary>
+    [AddComponentMenu("")]
     internal sealed class LateUpdateStrategy : UpdateStrategy
     {
         private void LateUpdate()

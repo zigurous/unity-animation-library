5b6fb2d2d73eab7673d2580667c7333e91e599df
Adam Graham <adam@zigurous.com>
2026-02-24 16:22:53 -0600

    Conform ShaderProperty to IEquatable

--- Runtime/DataStructures/ShaderProperty.cs
+++ Runtime/DataStructures/ShaderProperty.cs
@@ -8,7 +8,7 @@ namespace Zigurous.Animation
     /// created for the property for optimal code.
     /// </summary>
     [Serializable]
-    public struct ShaderProperty
+    public struct ShaderProperty : IEquatable<ShaderProperty>
     {
         [NonSerialized]
         [HideInInspector]
@@ -53,6 +53,61 @@ namespace Zigurous.Animation
         {
             m_Name = name;
             m_Id = Shader.PropertyToID(name);
+        }
+
+        /// <summary>
+        /// Determines if the shader property is equal to another.
+        /// </summary>
+        /// <param name="other">The other shader property to compare to.</param>
+        /// <returns>True if the shader properties are equal, false otherwise.</returns>
+        public bool Equals(ShaderProperty other)
+        {
+            return id == other.id;
+        }
+
+        /// <summary>
+        /// Determines if the shader property is equal to another.
+        /// </summary>
+        /// <param name="other">The other object to compare to.</param>
+        /// <returns>True if the shader properties are equal, false otherwise.</returns>
+        public override bool Equals(object other)
+        {
+            if (other is ShaderProperty property) {
+                return Equals(property);
+            } else {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Returns the hash code of the shader property.
+        /// </summary>
+        /// <returns>The hash code of the shader property.</returns>
+        public override int GetHashCode()
+        {
+            return id.GetHashCode();
+        }
+
+        /// <summary>
+        /// Determines if two shader properties are equal.
+        /// </summary>
+        /// <param name="lhs">The first shader property to compare.</param>
+        /// <param name="rhs">The second shader property to compare.</param>
+        /// <returns>True if the shader properties are equal, false otherwise.</returns>
+        public static bool operator ==(ShaderProperty lhs, ShaderProperty rhs)
+        {
+            return lhs.Equals(rhs);
+        }
+
+        /// <summary>
+        /// Determines if two shader properties are not equal.
+        /// </summary>
+        /// <param name="lhs">The first shader property to compare.</param>
+        /// <param name="rhs">The second shader property to compare.</param>
+        /// <returns>True if the shader properties are not equal, false otherwise.</returns>
+        public static bool operator !=(ShaderProperty lhs, ShaderProperty rhs)
+        {
+            return !lhs.Equals(rhs);
         }
 
         /// <summary>
